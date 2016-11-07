ff2e78c74f8a0bbc44409cf6e3084bb94bf139e1
emusute <miyanishi1212@gmail.com>
2016-11-08 00:54:51 +0900

    UserStreamの適応ついでに再設計。まだ限界を超えたツイートを試してはない。

--- main.go
+++ main.go
@@ -2,270 +2,145 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
-	"os"
-	"os/signal"
+	"net/url"
 	"strconv"
-	"syscall"
-	"time"
 
+	"github.com/ChimeraCoder/anaconda"
 	"github.com/kurehajime/dajarep"
-	"github.com/mrjones/oauth"
 )
 
-type Twitter struct {
-	consumer    *oauth.Consumer
-	accessToken *oauth.AccessToken
+type ApiConf struct {
+	ConsumerKey       string `json:"consumer_key"`
+	ConsumerSecret    string `json:"consumer_secret"`
+	AccessToken       string `json:"access_token"`
+	AccessTokenSecret string `json:"access_token_secret"`
 }
 
-func NewTwitter(consumerKey, consumerSecret, accessToken, accessTokenSecret string) *Twitter {
-	twitter := new(Twitter)
-	twitter.consumer = oauth.NewConsumer(
-		consumerKey,
-		consumerSecret,
-		oauth.ServiceProvider{
-			RequestTokenUrl:   "http://api.twitter.com/oauth/request_token",
-			AuthorizeTokenUrl: "https://api.twitter.com/oauth/authorize",
-			AccessTokenUrl:    "https://api.twitter.com/oauth/access_token",
-		})
-	twitter.accessToken = &oauth.AccessToken{accessToken, accessTokenSecret, nil}
-	// twitter.accessToken = &oauth.AccessToken{accessToken, accessTokenSecret}
-	return twitter
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
-
-func (t *Twitter) Get(url string, params map[string]string) (interface{}, error) {
-	response, err := t.consumer.Get(url, params, t.accessToken)
-	if err != nil {
-		return nil, err
-	}
-	defer response.Body.Close()
-
-	b, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	// decode
-	var result interface{}
-	err = json.Unmarshal(b, &result)
-	return result, err
-}
-
-func (t *Twitter) Post(url string, params map[string]string) (interface{}, error) {
-	response, err := t.consumer.Post(url, params, t.accessToken)
-	if err != nil {
-		return nil, err
-	}
-	defer response.Body.Close()
-
-	b, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	// decode
-	var result interface{}
-	err = json.Unmarshal(b, &result)
-	return result, err
-}
-
-// func checkSig(twitter *Twitter) {
-//
-// 	code := <-exitChan
-// 	os.Exit(code)
-// }
 
 func main() {
-	// fmt.Println("検出開始です。")
-	twitter := NewTwitter("3PNBbgWuPYMAPsJ3lHLuu9E29", "eQxU7jrfpcUiV4O4dpRBfLWMAsS8rTTZkqLehyWB2dGHDS5Ta5", "3527859379-naqp2WQMAXOL1gkmJZL6ILaQUxPnnjJLpGFAWUU", "JWd84aTCAenBgqWytq60hzkkMesgwo3qRRMiyoBHVY046")
-	oldTweetID := " "
-	dajareCount := 0
-	var newTweet interface{}
-	var tweet map[string]interface{}
-
-	//ダジャレ検出開始通知
-	_, error := twitter.Post(
-		"https://api.twitter.com/1.1/statuses/update.json",
-		map[string]string{"status": "@CaroBays ダジャレ検出開始します。"})
-
-	for i := 1; error != nil; i++ {
-		_, error = twitter.Post(
-			"https://api.twitter.com/1.1/statuses/update.json",
-			map[string]string{"status": "@CaroBays ダジャレ検出開始します。" + strconv.Itoa(i) + "回目"})
+	var apiConf ApiConf
+	{
+		apiConfPath := flag.String("conf", "config.json", "API Config File")
+		flag.Parse()
+		data, err_file := ioutil.ReadFile(*apiConfPath)
+		check(err_file)
+		err_json := json.Unmarshal(data, &apiConf)
+		check(err_json)
 	}
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan,
-		syscall.SIGHUP,
-		syscall.SIGINT,
-		syscall.SIGTERM,
-		syscall.SIGQUIT)
+	anaconda.SetConsumerKey(apiConf.ConsumerKey)
+	anaconda.SetConsumerSecret(apiConf.ConsumerSecret)
+	api := anaconda.NewTwitterApi(apiConf.AccessToken, apiConf.AccessTokenSecret)
+	api.SetLogger(anaconda.BasicLogger) // logger を設定
 
-	go func() {
-		for {
-			s := <-signalChan
-			switch s {
-			// kill -SIGHUP XXXX
-			case syscall.SIGHUP:
-				fmt.Println("hungup")
+	v := url.Values{}
 
-			// kill -SIGINT XXXX or Ctrl+c
-			case syscall.SIGINT:
-				fmt.Println("Warikomi")
-				_, error := twitter.Post(
-					"https://api.twitter.com/1.1/statuses/update.json",
-					map[string]string{"status": "@CaroBays ダジャレの検出を終了します。"})
-				// _, error := twitter.Post(
-				// 	"https://api.twitter.com/1.1/statuses/update.json",
-				// 	map[string]string{"status": "ダジャレの検出を終了します。"})
+	stream := api.UserStream(v)
+	//検出ツイート
+	// _,err:=api.PostTweet("@CaroBays ダジャレ検出を開始します。",nil)
+	_, err := api.PostTweet("テスト開始通知。", nil)
 
-				for i := 1; error != nil; i++ {
-					_, error = twitter.Post(
-						"https://api.twitter.com/1.1/statuses/update.json",
-						map[string]string{"status": "ダジャレの検出を終了します。" + strconv.Itoa(i) + "回目"})
-				}
-				os.Exit(0)
+	//すでに検出ツイートをしていた場合
+	for i := 2; err != nil; i++ {
+		_, err = api.PostTweet(strconv.Itoa(i)+"回目のテスト開始通知。", nil)
+	}
 
-			// kill -SIGTERM XXXX
-			case syscall.SIGTERM:
-				fmt.Println("force stop")
-				_, error := twitter.Post(
-					"https://api.twitter.com/1.1/statuses/update.json",
-					map[string]string{"status": "@CaroBays ダジャレの検出を終了します。"})
-				// _, error := twitter.Post(
-				// 	"https://api.twitter.com/1.1/statuses/update.json",
-				// 	map[string]string{"status": "ダジャレの検出を終了します。"})
+	//変数の準備
+	var tweet string
+	var userName string
+	var tweetID string
+	var userID string
 
-				for i := 1; error != nil; i++ {
-					_, error = twitter.Post(
-						"https://api.twitter.com/1.1/statuses/update.json",
-						map[string]string{"status": "ダジャレの検出を終了します。" + strconv.Itoa(i) + "回目"})
-				}
-				os.Exit(0)
-
-			// kill -SIGQUIT XXXX
-			case syscall.SIGQUIT:
-				fmt.Println("stop and core dump")
-				_, error := twitter.Post(
-					"https://api.twitter.com/1.1/statuses/update.json",
-					map[string]string{"status": "@CaroBays ダジャレの検出を終了します。"})
-				// _, error := twitter.Post(
-				// 	"https://api.twitter.com/1.1/statuses/update.json",
-				// 	map[string]string{"status": "ダジャレの検出を終了します。"})
-
-				for i := 1; error != nil; i++ {
-					_, error = twitter.Post(
-						"https://api.twitter.com/1.1/statuses/update.json",
-						map[string]string{"status": "ダジャレの検出を終了します。" + strconv.Itoa(i) + "回目"})
-				}
-				os.Exit(0)
-
-			default:
-				fmt.Println("Unknown signal.")
-				_, error := twitter.Post(
-					"https://api.twitter.com/1.1/statuses/update.json",
-					map[string]string{"status": "@CaroBays ダジャレの検出を終了します。"})
-				// _, error := twitter.Post(
-				// 	"https://api.twitter.com/1.1/statuses/update.json",
-				// 	map[string]string{"status": "ダジャレの検出を終了します。"})
-
-				for i := 1; error != nil; i++ {
-					_, error = twitter.Post(
-						"https://api.twitter.com/1.1/statuses/update.json",
-						map[string]string{"status": "ダジャレの検出を終了します。" + strconv.Itoa(i) + "回目"})
-				}
-				os.Exit(1)
-			}
-		}
-	}()
-
-	fmt.Println("検出開始")
-
+	//ツイート検出のためのfor
 	for {
-		var myTweet interface{} = " "
-		// ホームタイムラインを取得
-		res, err := twitter.Get(
-			"https://api.twitter.com/1.1/statuses/home_timeline.json", // Resource URL
-			map[string]string{})                                       // Parameters
-		if err != nil {
-			_, error := twitter.Post(
-				"https://api.twitter.com/1.1/statuses/update.json",
-				map[string]string{"status": "@CaroBays ダジャレの検出を終了します。"})
-			// _, error := twitter.Post(
-			// 	"https://api.twitter.com/1.1/statuses/update.json",
-			// 	map[string]string{"status": "ダジャレの検出を終了します。"})
+		select {
+		case item := <-stream.C:
+			switch status := item.(type) {
+			case anaconda.Tweet:
+				//ツイートしたユーザー
+				userName = status.User.ScreenName
+				//ツイートのID
+				tweetID = status.IdStr
+				//ツイートしたユーザーのID
+				userID = status.User.IdStr
 
-			for i := 1; error != nil; i++ {
-				_, error = twitter.Post(
-					"https://api.twitter.com/1.1/statuses/update.json",
-					map[string]string{"status": "ダジャレの検出を終了します。" + strconv.Itoa(i) + "回目"})
-			}
-		}
+				//自分のアカウント以外を読み込むようにする
+				if userID != "3527859379" {
+					tweet = status.Text
+					// Tweet を受信
+					fmt.Println(userName + ":" + tweet)
 
-		//初回起動時にoldTweetIDにIDをセットする
-		if oldTweetID == " " {
-			// 最新ツイートの入手
-			newTweet = res.([]interface{})[0]
-			tweet, _ = newTweet.(map[string]interface{})
-			oldTweetID = tweet["id_str"].(string)
-		}
+					// ダジャレがあるか確かめる
+					result, daj, wad := checkDajare(tweet)
 
-		//最新ツイート全ての読み込み
-		for i := 0; ; i++ {
-			//最新ツイートの読み込み
-			newTweet = res.([]interface{})[i]
-			tweet, _ = newTweet.(map[string]interface{})
+					//ダジャレがある時
+					if result {
+						//ダジャレ検出ツイート
+						text := makeTweet(userName, daj, wad)
 
-			if tweet["id_str"].(string) != oldTweetID {
-				//ツイートの内容を変数に格納
-				text := tweet["text"]
-
-				//ツイートの表示
-				fmt.Println(tweet["text"])
-
-				//interface{}型からstringへの変換が可能な場合、okはtrueとなる
-				if str, ok := text.(string); ok {
-					dajare, kana := dajarep.Dajarep(str)
-					fmt.Println(dajare)
-					fmt.Println(kana)
-					// ユーザデータを変数に格納
-					user := tweet["user"].(map[string]interface{})
-
-					if len(dajare) >= 1 {
-						dajareCount++
-						output := ""
-						for i := 0; i < len(dajare); i++ {
-							output += "@" + user["screen_name"].(string) + " @CaroBays " + "\"" + dajare[i] + "\" から \"" + kana[i] + "\""
-							if i < len(dajare)-1 {
-								output += "と"
-							} else {
-								output += "を検出しました。\n検出開始から" + strconv.Itoa(dajareCount) + "回目。"
-							}
-						}
-						if len([]rune(output)) <= 140 {
-							// ダジャレの検出ツイート
-							myTweet, _ = twitter.Post(
-								"https://api.twitter.com/1.1/statuses/update.json",
-								map[string]string{"status": output, "in_reply_to_status_id": tweet["id_str"].(string)})
-						} else {
-							// ダジャレの検出ツイート
-							myTweet, _ = twitter.Post(
-								"https://api.twitter.com/1.1/statuses/update.json",
-								map[string]string{"status": "@" + user["screen_name"].(string) + " @CaroBays " + "ダジャレを検出しました。\n検出開始から" + strconv.Itoa(dajareCount) + "回目。", "in_reply_to_status_id": tweet["id_str"].(string)})
-						}
+						//リプライを送る相手を指定
+						v.Set("in_reply_to_status_id", tweetID)
+						//ツイート送信
+						_, error := api.PostTweet(text, v)
+						fmt.Println(error)
 					}
 				}
-			} else {
-				if _, ok := myTweet.(string); !ok {
-					oldTweetID = myTweet.(map[string]interface{})["id_str"].(string)
-				} else {
-					oldTweetID = res.([]interface{})[0].(map[string]interface{})["id_str"].(string)
-				}
-				break
+
+			default:
 			}
 		}
-		time.Sleep(65000 * time.Millisecond)
+	}
+}
+
+//ダジャレがあるか確かめるメソッド
+func checkDajare(text string) (bool, []string, []string) {
+	//ダジャレとかかっているワードの検出
+	dajare, ward := dajarep.Dajarep(text)
+	//かかっているワードが1以上の時trueとなる
+	result := len(dajare) >= 1
+
+	return result, dajare, ward
+}
+
+//ダジャレ検出ツイートを作るメソッド
+func makeTweet(toName string, dajare []string, ward []string) string {
+	//変数の準備
+	var content string
+	var output string
+
+	//検出ツイートの際に巻き込まれるユーザー
+	targetUser := " @CaroBays"
+
+	//検出したダジャレ全てを通知するために一つのツイートに全てまとめる
+	for i := 0; i < len(dajare); i++ {
+		//ダジャレをコンソールへ出力
+		fmt.Println(dajare[i] + "," + ward[i])
+
+		//検出内容の格納
+		content += " \"" + dajare[i] + "\" から \"" + ward[i] + "\""
+
+		if i < len(dajare)-1 {
+			content += "と"
+		} else {
+			content += "を検出しました。"
+		}
 	}
 
+	//ツイート制限140文字を守るための条件文(初めの@の分も考慮して140「未満」としてある)
+	if len([]rune(content))+len([]rune(targetUser))+len([]rune(toName)) < 140 {
+		content = " ダジャレを検出しました。"
+	}
+
+	//実際に出力される文章
+	output = "@" + toName + targetUser + content
+
+	return output
 }
